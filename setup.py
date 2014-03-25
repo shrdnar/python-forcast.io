7b591a3282237932510aeac3f958a429ae5c694e
Kyle Cummings <kyle@welikesmall.com>
2014-03-25 12:40:09 -0600

    use shrdnar fork

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "python-forecastio",
+    name = "django-forecastio",
     version = "0.1",
     author = "Ze'ev Gilovitz",
     author_email = "zeev.gil@gmail.com",
     description = ("A thin Python Wrapper for the Forecast.io weather API"),
     license = "BSD 2-clause",
     keywords = "weather API wrapper forecast.io location",
-    url = "https://github.com/ZeevG/python-forcast.io",
+    url = "https://github.com/shrdnar/python-forcast.io",
     packages=['forecastio'],
     package_data={'forecastio': ['LICENSE.txt', 'README.rst']},
     long_description=open('README.rst').read(),
